5e77299b9d150c5b98e1b48073242b2ae4e78bad
s-c-sanchez <sercassan@gmail.com>
2026-05-24 00:08:46 +0200

    Add tests for number schema

---
+++ test/schemas/number.test.ts
@@ -0,0 +1,50 @@
+import { describe, expect, it } from "vitest"
+import { number, type SafeParseFail, safeParse } from "@/index"
+
+describe.concurrent("Number schema", () => {
+  const schema = number()
+
+  it("should have correct properties", () => {
+    expect(schema.type).toBe("number")
+    expect(schema.isAsync).toBe(false)
+  })
+
+  it("should parse successfully", () => {
+    expect(safeParse(schema, 12).success).toBe(true)
+  })
+
+  it.each(["john", true, null, undefined, {}, []])("should parse with issues", value => {
+    expect(safeParse(schema, value).success).toBe(false)
+  })
+
+  it("should return correct issue", () => {
+    const result = safeParse(schema, "john")
+
+    expect(result).toEqual({
+      success: false,
+      issues: [
+        {
+          isFatal: true,
+          message: "Invalid input",
+          path: [],
+        },
+      ],
+    } satisfies SafeParseFail)
+  })
+
+  it("should return custom error message", () => {
+    const customSchema = number("Custom error")
+    const result = safeParse(customSchema, "john")
+
+    expect(result).toEqual({
+      success: false,
+      issues: [
+        {
+          isFatal: true,
+          message: "Custom error",
+          path: [],
+        },
+      ],
+    } satisfies SafeParseFail)
+  })
+})
