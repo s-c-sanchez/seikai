d88e995d759c9f25a13165f5a39987fbea9cebdb
s-c-sanchez <sercassan@gmail.com>
2026-05-24 20:57:14 +0200

    Add tsdocs

--- src/schemas/date.ts
+++ src/schemas/date.ts
@@ -1,10 +1,23 @@
 import type { Schema } from "@/types/schemas"
 import { addIssue } from "@/utils/issue"
 
+/**
+ * Represents a date schema.
+ */
 export interface DateSchema extends Schema<Date> {
+  /**
+   * The specific schema type identifier.
+   */
   readonly type: "date"
 }
 
+/**
+ * Creates a date schema.
+ *
+ * @param message - An optional custom error message to use when validation fails.
+ *
+ * @returns A new date schema.
+ */
 export function date(message?: string): DateSchema {
   return {
     "~run": (input, ctx, path) => {
