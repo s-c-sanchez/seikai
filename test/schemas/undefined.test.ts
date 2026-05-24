c45f38d821cd135bb3bb8244643b673bed7703ba
s-c-sanchez <sercassan@gmail.com>
2026-05-24 20:37:21 +0200

    Add tests for undefined schema

---
+++ test/schemas/undefined.test.ts
@@ -0,0 +1,55 @@
+import { describe, expect, expectTypeOf, it } from "vitest"
+import { parse, type SafeParseFail, undefined as s_undefined, safeParse } from "@/index"
+
+describe.concurrent("Undefined schema", () => {
+  const schema = s_undefined()
+
+  it("should have correct properties", () => {
+    expect(schema.type).toBe("undefined")
+    expect(schema.isAsync).toBe(false)
+  })
+
+  it("should parse successfully", () => {
+    expect(safeParse(schema, undefined).success).toBe(true)
+  })
+
+  it("should return correct typescript type", () => {
+    const result = parse(schema, undefined)
+    expectTypeOf(result).toEqualTypeOf<undefined>()
+  })
+
+  it.each(["john", 12, 12n, true, null, {}, []])("should parse with issues", value => {
+    expect(safeParse(schema, value).success).toBe(false)
+  })
+
+  it("should return correct issue", () => {
+    const result = safeParse(schema, "john")
+
+    expect(result).toEqual({
+      success: false,
+      issues: [
+        {
+          isFatal: true,
+          message: "Invalid input",
+          path: [],
+        },
+      ],
+    } satisfies SafeParseFail)
+  })
+
+  it("should return custom error message", () => {
+    const customSchema = s_undefined("Custom error")
+    const result = safeParse(customSchema, "john")
+
+    expect(result).toEqual({
+      success: false,
+      issues: [
+        {
+          isFatal: true,
+          message: "Custom error",
+          path: [],
+        },
+      ],
+    } satisfies SafeParseFail)
+  })
+})
