d1f24d87a673ee7be334c4044c72449d7b8d4f11
s-c-sanchez <sercassan@gmail.com>
2026-05-24 19:41:06 +0200

    Add bigint to test cases

--- test/schemas/bigint.test.ts
+++ test/schemas/bigint.test.ts
@@ -1,29 +1,29 @@
 import { describe, expect, expectTypeOf, it } from "vitest"
-import { parse, type SafeParseFail, safeParse, string } from "@/index"
+import { bigint, parse, type SafeParseFail, safeParse } from "@/index"
 
-describe.concurrent("String schema", () => {
-  const schema = string()
+describe.concurrent("Bigint schema", () => {
+  const schema = bigint()
 
   it("should have correct properties", () => {
-    expect(schema.type).toBe("string")
+    expect(schema.type).toBe("bigint")
     expect(schema.isAsync).toBe(false)
   })
 
   it("should parse successfully", () => {
-    expect(safeParse(schema, "john").success).toBe(true)
+    expect(safeParse(schema, 12n).success).toBe(true)
   })
 
   it("should return correct typescript type", () => {
-    const result = parse(schema, "john")
-    expectTypeOf(result).toEqualTypeOf<string>()
+    const result = parse(schema, 12n)
+    expectTypeOf(result).toEqualTypeOf<bigint>()
   })
 
-  it.each([12, 12n, true, null, undefined, {}, []])("should parse with issues", value => {
+  it.each(["john", 12, true, null, undefined, {}, []])("should parse with issues", value => {
     expect(safeParse(schema, value).success).toBe(false)
   })
 
   it("should return correct issue", () => {
-    const result = safeParse(schema, 12)
+    const result = safeParse(schema, "john")
 
     expect(result).toEqual({
       success: false,
@@ -38,8 +38,8 @@ describe.concurrent("String schema", () => {
   })
 
   it("should return custom error message", () => {
-    const customSchema = string("Custom error")
-    const result = safeParse(customSchema, 12)
+    const customSchema = bigint("Custom error")
+    const result = safeParse(customSchema, "john")
 
     expect(result).toEqual({
       success: false,
