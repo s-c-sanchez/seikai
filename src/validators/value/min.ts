f79a037afb5f5a54ba48fcd13f5971a6736cc712
s-c-sanchez <sercassan@gmail.com>
2026-05-28 00:27:49 +0200

    Add validator actions for comparable values

---
+++ src/validators/value/min.ts
@@ -0,0 +1,19 @@
+import type { Action } from "@/types/actions"
+import type { IssueInput } from "@/types/issues"
+import { addIssue } from "@/utils/issue"
+
+export function min<TType extends number | bigint | Date>(
+  value: NoInfer<TType>,
+  issue?: IssueInput | string,
+): Action<TType> {
+  return {
+    "~run": (input, ctx, path) => {
+      if (input < value) {
+        addIssue(ctx, path, issue)
+      }
+
+      return input
+    },
+    isAsync: false,
+  }
+}
