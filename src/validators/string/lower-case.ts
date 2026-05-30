2135791094db77d4b17a0837fb6a3d6ff9addf56
s-c-sanchez <sercassan@gmail.com>
2026-05-30 17:33:45 +0200

    Change file convention of lower and upper casing validators

--- src/validators/string/lower-case.ts
+++ src/validators/string/lower-case.ts
@@ -2,7 +2,7 @@ import type { Action } from "@/types/actions"
 import type { IssueInput } from "@/types/issues"
 import { addIssue } from "@/utils/issue"
 
-export function lowercase(issue?: IssueInput | string): Action<string> {
+export function lowerCase(issue?: IssueInput | string): Action<string> {
   return {
     "~run": (input, ctx, path) => {
       if (input !== input.toLowerCase()) {
--- src/validators/string/upper-case.ts
+++ src/validators/string/upper-case.ts
@@ -2,7 +2,7 @@ import type { Action } from "@/types/actions"
 import type { IssueInput } from "@/types/issues"
 import { addIssue } from "@/utils/issue"
 
-export function uppercase(issue?: IssueInput | string): Action<string> {
+export function upperCase(issue?: IssueInput | string): Action<string> {
   return {
     "~run": (input, ctx, path) => {
       if (input !== input.toUpperCase()) {
